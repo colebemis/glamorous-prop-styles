d6c433050849fe96e40146647464b430ebe5de34
Cole Bemis <colebemis@gmail.com>
2017-08-17 21:13:15 -0700

    chore: Add eslint

--- src/prop-styles.js
+++ src/prop-styles.js
@@ -13,7 +13,7 @@
  */
 function propStyles(styles) {
   return function dynamicStyles(props) {
-    return Object.keys(props).map(function(key) {
+    return Object.keys(props).map(function (key) {
       if (props[key]) {
         return typeof styles[key] === 'function'
           ? styles[key](props)
